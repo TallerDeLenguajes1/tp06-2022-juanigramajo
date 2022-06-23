106666d2d56f9d1cf13e7c3acd53e851c7019404
Juani <juanigramajo@hotmail.com>
2022-06-23 13:48:08 -0300

    carga de datos y subo la rama al rep

---
+++ MyApp/Empleado.cs
@@ -0,0 +1,99 @@
+
+public enum Cargo
+{
+    Auxiliar,
+    Administrativo, 
+    Ingeniero,
+    Especialista, 
+    Investigador
+}
+
+
+public class Empleado
+{
+    public string Nombre;
+    public string Apellido;
+    public DateTime FechaNac;
+    public char EstCivil;
+    public char Genero;
+    public DateTime FechaIng;
+    public double SueldoBasic;
+
+    public Cargo Cargos;
+
+    DateTime Now = DateTime.Today;
+    public int Edad = 0, AntiguedadEmp = 0, Anos = 0;
+    public double SalarioEmp = 0;
+
+
+    public int Antiguedad(DateTime fechaIn)
+    {
+        int AntiguedadEmp = Now.Year - fechaIn.Year;
+
+        if(fechaIn.Month > Now.Month)
+        {
+            AntiguedadEmp = AntiguedadEmp-1;
+        }
+
+        return AntiguedadEmp;
+    }
+
+
+    public int calcularEdad(DateTime fechaNac)
+    {
+        int Edad = Now.Year - fechaNac.Year;
+
+        if(fechaNac.Month > Now.Month)
+        {
+            Edad = Edad-1;
+        }
+
+        return Edad;
+    }
+
+
+    public int AnosPJubilarse(char genero)
+    {
+
+        if((genero == 'f') || (genero == 'F'))
+        {
+            Anos = 60 - Edad;
+        } else
+        {
+            Anos = 65 - Edad;
+        }
+
+        return Anos;
+    }
+
+
+    public double Salario(double sueldo, Cargo Cargo, char EstCivil, int antiguedad)
+    {
+
+        double adicional = 0;
+
+        if(antiguedad < 20)
+        {
+            adicional = (antiguedad * 0.01) * sueldo;
+        } else
+        {
+            adicional = 0.25 * sueldo;
+        }
+
+        if((Cargos == Cargo.Ingeniero) || (Cargos == Cargo.Especialista))
+        {
+            adicional = adicional * 1.50;
+        }
+
+        if((EstCivil == 'c') || (EstCivil == 'C'))
+        {
+            adicional = adicional + 15000;
+        }
+
+        SalarioEmp = sueldo + adicional;
+
+
+        return SalarioEmp;
+    }
+}
+
--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,66 +1,174 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
+using System;
 
 
-Calculadora calc = new Calculadora(0);
+Empleado[] empleados = new Empleado[3];
+double totalEnSalarios = 0;
 
-int opcion = 0;
-double num = 0;
-opcion = menu();
 
-while (opcion != 6){
-
-    if(opcion == 1 || opcion == 2 || opcion == 3 || opcion == 4){
-        Console.WriteLine("\nIngrese un numero: ");
-    num = Convert.ToDouble(Console.ReadLine());
-    }
-
-    string operac = " ";
-
-    switch (opcion){
-        case 1:
-            calc.Sumar(num);
-            operac = "sumar";
-            break;
-        case 2:
-            calc.Restar(num);
-            operac = "restar";
-            break;
-        case 3:
-            calc.Multiplicar(num);
-            operac = "multiplicar";
-            break;
-        case 4:
-            calc.Dividir(num);
-            operac = "dividir";
-            break;
-        case 5:
-            calc.Limpiar();
-            operac = "limpiar";
-            break;
-        default:
-            break;
-    }
-
-    Console.WriteLine("\n\n-----Resultado de " + operac + ": [" + calc.resultado + "]-----");
-
-    opcion = menu();
+for (int i = 0; i < 1; i++)
+{
+    Empleado emp = cargaEmpleados(i);
+    
+    empleados[i] = emp;
 }
 
 
 
 
-int menu(){
+Empleado cargaEmpleados(int ID)
+{
+    Empleado emp = new Empleado();
 
-    Console.WriteLine("\nOPCIONES:\n[1]: Suma\n[2]: Resta\n[3]: Multiplicación\n[4]: División\n[5]: Limpiar\n[6]: Salir\nIngrese que operación desea realizar: ");
-    opcion = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"\n----------CARGA DE DATOS----------");
+    Console.WriteLine($"\n-----Empleado [{ID+1}]-----");
+
+    Console.WriteLine("\nNombre:");
+    emp.Nombre = Console.ReadLine();
+
+    Console.WriteLine("\nApellido:");
+    emp.Apellido = Console.ReadLine();
 
 
-    while (opcion > 6){
 
-        Console.WriteLine("\nError de formato\nOPCIONES:\n[1]: Suma\n[2]: Resta\n[3]: Multiplicación\n[4]: División\n[5]: Limpiar\n[6]: Salir\nIngrese que operación desea realizar: ");
-        opcion = Convert.ToInt32(Console.ReadLine());
+    //-------------------Día de nacimiento-------------------
+    Console.Write("\n\nDía de nacimiento: ");
+    int dia = int.Parse(Console.ReadLine());
+
+    while ((dia < 0) ||  (dia > 31))
+    {
+        Console.Write("\nError de formato.\nDía de nacimiento: ");
+        dia = int.Parse(Console.ReadLine());
     }
 
-    return opcion;
+    //-------------------Mes de nacimiento-------------------
+    Console.Write("\nMes de nacimiento: ");
+    int mes = int.Parse(Console.ReadLine());
+
+    while ((mes < 0) ||  (mes > 12))
+    {
+        Console.Write("\nError de formato.\nMes de nacimiento: ");
+        mes = int.Parse(Console.ReadLine());
+    }
+
+    //-------------------Año de nacimiento-------------------
+    Console.Write("\nAño de nacimiento: ");
+    int ano = int.Parse(Console.ReadLine());
+
+    while ((ano < 1000) ||  (ano > 2022))
+    {
+        Console.Write("\nError de formato.\nAño de nacimiento: ");
+        ano = int.Parse(Console.ReadLine());
+    }
+
+    DateTime FechaNac = new DateTime(ano, mes, dia);
+
+
+
+    //-------------------Estado civil-------------------
+    Console.WriteLine("\n\nOpciones de estado civil:\nCasado [C]\nSoltero [S]\nIngrese una opción:");
+    emp.EstCivil = Char.ToLower(Convert.ToChar(Console.ReadLine()));
+
+    while ((emp.EstCivil != 'c') && (emp.EstCivil != 's'))
+    {
+        Console.WriteLine("\nError de formato.\nOpciones de estado civil:\nCasado [C]\nSoltero [S]\nIngrese una opción:");
+        emp.EstCivil = Char.ToLower(Convert.ToChar(Console.ReadLine()));
+    }
+
+
+
+    //-------------------Género-------------------
+    Console.WriteLine("\n\nOpciones de género:\nFemenino [F]\nMasculino [M]\nIngrese una opción:");
+    emp.Genero = Char.ToLower(Convert.ToChar(Console.ReadLine()));
+
+    while ((emp.Genero != 'm') && (emp.Genero != 'f'))
+    {
+        Console.WriteLine("\nError de formato.\nOpciones de género:\nFemenino [F]\nMasculino [M]\nIngrese una opción:");
+        emp.Genero = Char.ToLower(Convert.ToChar(Console.ReadLine()));
+    }
+
+
+
+    //-------------------Día de ingreso a la empresa-------------------
+    Console.Write("\n\nDía de ingreso a la empresa:");
+    int diaIng = int.Parse(Console.ReadLine());
+
+    while ((diaIng < 0) ||  (diaIng > 31))
+    {
+        Console.Write("\nError de formato.\nDía de ingreso a la empresa:");
+        diaIng = int.Parse(Console.ReadLine());
+    }
+
+    //-------------------Mes de ingreso a la empresa-------------------
+    Console.Write("\nMes de ingreso a la empresa:");
+    int mesIng = int.Parse(Console.ReadLine());
+
+    while ((mesIng < 0) ||  (mesIng > 12))
+    {
+        Console.Write("\nError de formato.\nMes de ingreso a la empresa:");
+        mesIng = int.Parse(Console.ReadLine());
+    }
+
+    //-------------------Año de ingreso a la empresa-------------------
+    Console.Write("\nAño de ingreso a la empresa:");
+    int anoIng = int.Parse(Console.ReadLine());
+
+    while ((anoIng < 1000) ||  (anoIng > 2022))
+    {
+        Console.Write("\nError de formato.\nAño de ingreso a la empresa:");
+        anoIng = int.Parse(Console.ReadLine());
+    }
+
+    DateTime FechaIng = new DateTime(anoIng, mesIng, diaIng);
+
+
+
+    Console.WriteLine("\n\nSalario:");
+    emp.SalarioEmp = Convert.ToDouble(Console.ReadLine());
+
+
+
+    //-------------------Cargo-------------------
+    Console.WriteLine("\n\nOpciones de cargos:\nAuxliar [0]\nAdministrativo [1]\nIngeniero [2]\nEspecialista [3]\nInvestigador [4]\nIngrese una opción:");
+    int dato = Convert.ToInt32(Console.ReadLine());
+
+    while ((dato < 0) && (dato > 4))
+    {
+        Console.WriteLine("\nError de formato.\nOpciones de cargos:\nAuxliar [0]\nAdministrativo [1]\nIngeniero [2]\nEspecialista [3]\nInvestigador [4]\nIngrese una opción:");
+        dato = Convert.ToInt32(Console.ReadLine());
+    };
+
+    switch (dato){
+        case 1:
+            emp.Cargos = Cargo.Auxiliar;
+            break;
+        case 2:
+            emp.Cargos = Cargo.Administrativo;
+            break;
+        case 3:
+            emp.Cargos = Cargo.Ingeniero;
+            break;
+        case 4:
+            emp.Cargos = Cargo.Especialista;
+            break;
+        case 5:
+            emp.Cargos = Cargo.Investigador;
+            break;
+        default:
+            break;
+    }
+
+    emp.Edad = emp.calcularEdad(emp.FechaNac);
+    
+    emp.AntiguedadEmp = emp.Antiguedad(emp.FechaIng);
+    
+    emp.Anos = emp.AnosPJubilarse(emp.Genero);
+    
+    emp.SalarioEmp = emp.Salario(emp.SalarioEmp, emp.Cargos, emp.EstCivil, emp.AntiguedadEmp);
+    
+    totalEnSalarios = totalEnSalarios + emp.SalarioEmp;
+
+
+    return emp;
 }
